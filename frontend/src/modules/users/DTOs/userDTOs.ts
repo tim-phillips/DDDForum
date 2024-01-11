9239b6ab2ec1de7386b0f61fb41117951f29139b
Tim Phillips <timphillips@hey.com>
2024-01-11 10:00:38 -0800

    test(frontend): scaffold e2e tests

---
+++ frontend/src/modules/users/DTOs/userDTOs.ts
@@ -0,0 +1,8 @@
+// TODO deduplicate
+
+export type CreateUserInput = {
+  firstName: string;
+  lastName: string;
+  username: string;
+  email: string;
+};
---
+++ frontend/tests/users/e2e/builders/UserBuilder.ts
@@ -0,0 +1,53 @@
+// TODO deduplicate
+
+import { faker } from "@faker-js/faker";
+
+import { CreateUserInput } from "../../../../src/modules/users/DTOs/userDTOs";
+
+export class UserBuilder {
+  private userInput: CreateUserInput;
+
+  constructor() {
+    const firstName = faker.person.firstName();
+    const lastName = faker.person.lastName();
+    const username = faker.internet.userName({
+      firstName,
+      lastName,
+    });
+    const email = faker.internet.email({
+      firstName,
+      lastName,
+      provider: "example.com",
+    });
+    this.userInput = {
+      firstName,
+      lastName,
+      username,
+      email,
+    };
+  }
+
+  withFirstName(value: string) {
+    this.userInput.firstName = value;
+    return this;
+  }
+
+  withLastName(value: string) {
+    this.userInput.lastName = value;
+    return this;
+  }
+
+  withUsername(value: string) {
+    this.userInput.username = value;
+    return this;
+  }
+
+  withEmail(value: string) {
+    this.userInput.email = value;
+    return this;
+  }
+
+  build(): CreateUserInput {
+    return this.userInput;
+  }
+}
