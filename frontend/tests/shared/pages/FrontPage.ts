c2b95c191dd367fb077fa71978fb37c50be73659
Tim Phillips <timphillips@hey.com>
2024-01-12 07:20:40 -0800

    test(frontend): add PageComponents

--- frontend/tests/shared/pages/FrontPage.ts
+++ frontend/tests/shared/pages/FrontPage.ts
@@ -1,14 +1,37 @@
 import { PuppeteerPageDriver } from "../drivers/PuppeteerPageDriver";
+import { PageComponents } from "./PageComponents/PageComponents";
 
 export class FrontPage {
-  constructor(private driver: PuppeteerPageDriver) {}
+  private pageDriver: PuppeteerPageDriver;
+  private pageComponents: PageComponents;
+
+  constructor(pageDriver: PuppeteerPageDriver) {
+    this.pageDriver = pageDriver;
+    this.pageComponents = new PageComponents({
+      driver: this.pageDriver,
+      elements: {
+        menu: { selector: "#menu", type: "div" },
+      },
+      timeout: 3000,
+    });
+  }
 
   async open() {
-    const page = await this.driver.getPage();
+    const page = await this.pageDriver.getPage();
     await page.goto("http://localhost:5173/");
   }
 
-  async clickJoin() {}
+  async clickJoin() {
+    await this.pageComponents.load();
+    const menu = this.pageComponents.get("menu");
+    await menu.click();
+  }
+
+  async getMenuText() {
+    await this.pageComponents.load();
+    const menu = this.pageComponents.get("menu");
+    return menu.evaluate((e) => e.textContent);
+  }
 
   async isOnPage() {
     return true;
---
+++ frontend/tests/shared/pages/PageComponents/PageComponents.ts
@@ -0,0 +1,73 @@
+import { ElementHandle } from "puppeteer";
+import { PuppeteerPageDriver } from "../../drivers/PuppeteerPageDriver";
+
+const DEFAULT_TIMEOUT = 3000;
+
+type ElementType = "div" | "button";
+
+interface Config {
+  elements: {
+    [key: string]: {
+      selector: string;
+      type: ElementType;
+    };
+  };
+  driver: PuppeteerPageDriver;
+  timeout?: number;
+}
+
+interface LoadedPageComponents {
+  [key: string]: {
+    selector: string;
+    type: ElementType;
+    element: ElementHandle<Element>;
+  };
+}
+
+export class PageComponents {
+  private loadedPageComponents: LoadedPageComponents;
+
+  constructor(private config: Config) {
+    this.loadedPageComponents = {};
+  }
+
+  async load() {
+    const newLoadedPageComponents: LoadedPageComponents = {};
+    const { driver, elements, timeout = DEFAULT_TIMEOUT } = this.config;
+    const page = await driver.getPage();
+    let element: ElementHandle<Element> | null;
+
+    for (const key in elements) {
+      const component = elements[key];
+      try {
+        element = await page.waitForSelector(component.selector, { timeout });
+      } catch (err) {
+        throw new Error(
+          `Could not load element '${key}', maybe it's not on the page yet.`
+        );
+      }
+      if (!element) {
+        throw new Error(
+          `Could not load element '${key}', maybe it's not on the page yet.`
+        );
+      }
+      newLoadedPageComponents[key] = { ...component, element };
+    }
+
+    this.loadedPageComponents = newLoadedPageComponents;
+  }
+
+  get(name: string): ElementHandle<Element> {
+    const isLoaded = Object.keys(this.loadedPageComponents).length;
+    if (!isLoaded) {
+      throw new Error(
+        "Page components not yet loaded, make sure to call `.load()`"
+      );
+    }
+    const element = this.loadedPageComponents[name].element;
+    if (element === undefined) {
+      throw new Error(`Component '${name}' not loaded`);
+    }
+    return element;
+  }
+}
