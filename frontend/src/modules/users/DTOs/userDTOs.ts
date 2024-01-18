d59726175df7b25f96f0a8099ce1aa5c516622b9
Tim Phillips <timphillips@hey.com>
2024-01-17 17:16:43 -0800

    feat(frontend): add UserService with createUser

--- frontend/src/modules/users/DTOs/userDTOs.ts
+++ frontend/src/modules/users/DTOs/userDTOs.ts
@@ -1,8 +1,40 @@
 // TODO deduplicate
 
+type Response<
+  Data extends unknown | undefined,
+  Success extends boolean,
+  Error extends null | string,
+> = {
+  data: Data;
+  success: Success;
+  error: Error;
+};
+
 export type CreateUserInput = {
+  email: string;
   firstName: string;
   lastName: string;
   username: string;
-  email: string;
 };
+
+export type UserDTO = {
+  id: number;
+  email: string;
+  firstName: string;
+  lastName: string;
+  username: string;
+};
+
+type CreateUserSuccess = Response<UserDTO, true, null>;
+
+type AccountAlreadyExists = Response<
+  undefined,
+  false,
+  "Account already exists"
+>;
+type UsernameTaken = Response<undefined, false, "Username taken">;
+
+export type CreateUserResponse =
+  | CreateUserSuccess
+  | AccountAlreadyExists
+  | UsernameTaken;
--- frontend/src/pages/RegistrationPage/RegistrationPage.tsx
+++ frontend/src/pages/RegistrationPage/RegistrationPage.tsx
@@ -1,13 +1,18 @@
 import { LayoutView } from "../../shared/components/Layout/LayoutView";
-import { NotificationService } from "../../shared/notifications/NotificationService";
 import { RegistrationFormView } from "./components/RegistrationFormView";
+import { NotificationService } from "../../shared/notifications/NotificationService";
+import { UserService } from "../../shared/users/UserService";
 
 export function RegistrationPage() {
   const notificationService = new NotificationService();
+  const userService = new UserService();
 
   return (
     <LayoutView title="Registration Page">
-      <RegistrationFormView notificationService={notificationService} />
+      <RegistrationFormView
+        notificationService={notificationService}
+        userService={userService}
+      />
     </LayoutView>
   );
 }
