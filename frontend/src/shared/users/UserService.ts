6c8e9e8f04b1662bcffe0f8b103b8bef7a257a45
Tim Phillips <timphillips@hey.com>
2024-02-07 10:14:17 -0800

    feat(frontend): set user into global state on createUser

--- frontend/src/shared/users/UserService.ts
+++ frontend/src/shared/users/UserService.ts
@@ -3,29 +3,34 @@ import axios from "axios";
 import {
   CreateUserInput,
   CreateUserResponse,
+  UserDTO,
 } from "../../modules/users/DTOs/userDTOs";
 
 export class UserService {
   async createUser(
     createUserInput: CreateUserInput,
-    onSuccess: () => void,
+    onSuccess: (user: UserDTO) => void,
     onError: (error: Error) => void
   ) {
     try {
       const response = await axios({
         method: "POST",
-        url: "https://localhost:3000/users/new",
+        url: "http://localhost:3030/users/new",
         data: createUserInput,
       });
 
       const createUserResponse: CreateUserResponse = response.data;
 
-      return onSuccess();
+      if (createUserResponse.success) {
+        console.log(createUserResponse);
+        return onSuccess(createUserResponse.data);
+      }
+      return onError(new Error("Unknown error"));
     } catch (error: unknown) {
       if (error instanceof Error) {
         return onError(error);
       }
-      throw error;
+      return onError(new Error("Unknown error"));
     }
   }
 }
