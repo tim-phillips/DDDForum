fbd6cdc61a80f7f53e59277c30bbf503018b3311
Tim Phillips <timphillips@hey.com>
2024-02-07 10:11:50 -0800

    feat(frontend): add GlobalStateProvider to App

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 
+import { GlobalStateProvider } from "./shared/globalState/GlobalStateProvider";
 import { NotificationService } from "./shared/notifications/NotificationService";
 import { MainPage } from "./pages/MainPage/MainPage";
 import { RegistrationPage } from "./pages/RegistrationPage/RegistrationPage";
@@ -9,7 +10,7 @@ import "./App.css";
 
 function App() {
   return (
-    <>
+    <GlobalStateProvider>
       <NotificationService.ToastContainer />
       <Router>
         <Routes>
@@ -17,7 +18,7 @@ function App() {
           <Route path="/register" element={<RegistrationPage />} />
         </Routes>
       </Router>
-    </>
+    </GlobalStateProvider>
   );
 }
 
